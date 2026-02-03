d879b5a11d8b5ef87eb2a4041cc6fa215a7cc774
bipasa-maji <bipasamaji_230127@aitpune.edu.in>
2026-02-04 01:22:11 +0530

    Defined models in orders app - Order, OrderItem and completed the models making

--- products/models.py
+++ products/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price= models.DecimalField(max_digits=30, decimal_places=2, default=0.00)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.title
     
--- stores/models.py
+++ stores/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from products.models import Product
 class Store(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=300)
@@ -8,10 +8,14 @@ class Store(models.Model):
         return self.name
 
 class Inventory(models.Model):
-    store = models.ForeignKey(Store,on_delete=models.CASCADE)
-    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='inventories')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='inventories')
+    quantity = models.PositiveIntegerField()
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['store', 'product'], name='unique_inventory_per_store_product')
+        ]
     def __str__(self):
-        return f"{self.product.title} in {self.store.name}"
+        return f"{self.store} - {self.product}"
     
 
