ed21c4c7e9ffc3fb4feeb406d48bb38792928986
bipasa-maji <bipasamaji_230127@aitpune.edu.in>
2026-02-04 00:53:32 +0530

    Defined models in stores app - Store, Inventory

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+class Inventory(models.Model):
+    store = models.ForeignKey(Store,on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return f"{self.product.title} in {self.store.name}"
+    
+
