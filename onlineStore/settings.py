d9ff35aff2c38ce76dcce6d243ceff2457c60e5f
bipasa-maji <bipasamaji_230127@aitpune.edu.in>
2026-02-04 01:40:11 +0530

    Shifted the database to postgres and made the migrations and registered the models top admin

--- onlineStore/settings.py
+++ onlineStore/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'rest_framework',
+    'stores',
     'products',
     'orders',
     'search',
@@ -73,14 +74,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'onlineStore.wsgi.application'
 
+#REST Framework Config
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aforro',
+        'USER': 'aforro_user',
+        'PASSWORD': 'securepassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
