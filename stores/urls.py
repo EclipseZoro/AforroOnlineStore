9fef4c8a60ceb477812285bb2c9159e8a30c90d9
bipasa-maji <bipasamaji_230127@aitpune.edu.in>
2026-02-04 13:40:39 +0530

    Made the URL routing for the order listing thorugh stores.urls and tested the working of it: its working fine

--- stores/urls.py
+++ stores/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from orders.views import StoreOrderListAPIView
 
 urlpatterns = [
+    path('stores/<int:store_id>/orders/', StoreOrderListAPIView.as_view()),
 ]
