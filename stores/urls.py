5cb0f04407051cdc9d2be4dece1a0f1cdfcb6504
bipasa-maji <bipasamaji_230127@aitpune.edu.in>
2026-02-04 13:47:57 +0530

    Made the inventory listing views and url routings in the stores app and tested: its working fine

--- stores/urls.py
+++ stores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import StoreInventoryListAPIView
 from orders.views import StoreOrderListAPIView
 
 urlpatterns = [
     path('stores/<int:store_id>/orders/', StoreOrderListAPIView.as_view()),
+    path('stores/<int:store_id>/inventory/', StoreInventoryListAPIView.as_view()),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Inventory
+from .serializers import StoreInventoryListSerializer
 
-# Create your views here.
+class StoreInventoryListAPIView(ListAPIView):
+    serializer_class = StoreInventoryListSerializer
+
+    def get_queryset(self):
+        store_id = self.kwargs["store_id"]
+
+        return (
+            Inventory.objects
+            .filter(store_id=store_id)
+            .select_related("product", "product__category")
+            .order_by("product__title")
+        )
